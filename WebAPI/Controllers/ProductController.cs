3c549f77a77a2e55c5b996be972f8248071f004a
FATIH <fthokn57@icloud.com>
2023-10-27 14:54:18 +0300

    Ioc (Inversion of Control ) ve autofac kullanımı

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        //ProductManager productManager = new ProductManager(new EfProductDal());
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+
+            if (result.Success == true)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result);
+
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
